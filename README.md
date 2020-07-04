f7c335ab75e439b22f16a6ca195f0ff75d49194d
gosuhiman <gosuhiman@gmail.com>
2020-07-05 00:20:38 +0200

    more approaches, increase filters, kernel size, max pooling

--- README.md
+++ README.md
@@ -8,3 +8,57 @@ model = Sequential([
     Dense(10, activation='softmax'),
 ])
 ```
+
+Approach 2 - `0.9751` accuracy
+```
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    Flatten(),
+    Dense(24, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+```
+
+Approach 3 - `0.9758` accuracy
+```
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(24, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+```
+
+Approach 4 - `0.9773` accuracy
+```
+model = Sequential([
+    Conv2D(48, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(24, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+```
+
+Approach 5 - `0.9821` accuracy
+```
+model = Sequential([
+    Conv2D(32, (7, 7), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(24, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+```
+
+Approach 6 - `0.9819` accuracy
+```
+model = Sequential([
+    Conv2D(48, (7, 7), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(24, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+```
--- train.py
+++ train.py
@@ -1,17 +1,18 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 
 # Paths
+
 model_path = 'models/'
 data_path = 'data/mnist/images/'
 train_path = data_path + 'train'
 
 image_size = (28, 28)
 classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-batch_size = 1000
+batch_size = 5
 epochs_count = 10
 
 train_data_generator = ImageDataGenerator(validation_split=0.1)
@@ -34,8 +35,10 @@ validation_directory_iterator = train_data_generator.flow_from_directory(
 )
 
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    Conv2D(48, (7, 7), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(),
     Flatten(),
+    Dense(24, activation='relu'),
     Dense(10, activation='softmax'),
 ])
 
@@ -45,10 +48,9 @@ model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentro
 model.fit(
     train_directory_iterator,
     validation_data=validation_directory_iterator,
-    steps_per_epoch=train_directory_iterator.samples / batch_size,
-    validation_steps=validation_directory_iterator.samples / batch_size,
-    epochs=epochs_count,
-    verbose=2
+    steps_per_epoch=train_directory_iterator.samples / epochs_count,
+    validation_steps=validation_directory_iterator.samples / epochs_count,
+    epochs=epochs_count
 )
 
 if not os.path.exists(model_path):
