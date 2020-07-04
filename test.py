2873d27e5b67e5c45105b21ddd83eec52dd70166
gosuhiman <gosuhiman@gmail.com>
2020-07-04 23:44:01 +0200

    train and test approach 1 on mnist - 0.9519 accuracy

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
-from create_sample_set import get_train_samples
 
+# Paths
 model_path = 'models/'
+data_path = 'data/mnist/images/'
+test_path = data_path + 'test'
+batch_size = 1000
+
+image_size = (28, 28)
+classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+test_batches = ImageDataGenerator().flow_from_directory(
+    test_path,
+    target_size=image_size,
+    classes=classes,
+    batch_size=batch_size,
+    color_mode='grayscale',
+    shuffle=False
+)
 
 model = load_model(model_path + 'model.h5')
 
-test_samples, test_labels = get_train_samples(200)
-predictions = model.predict(test_samples, batch_size=10, verbose=0)
+model.evaluate(test_batches)
+
+predictions = model.predict(test_batches)
 classified_predictions = np.argmax(predictions, axis=-1)
 
-cm = confusion_matrix(test_labels, classified_predictions)
+cm = confusion_matrix(test_batches.labels, classified_predictions)
 plt.figure()
 plt.title('Confusion Matrix')
 sn.heatmap(cm, cmap='coolwarm', annot=True, fmt='.5g')
--- train.py
+++ train.py
@@ -1,23 +1,55 @@
-import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-from create_sample_set import get_train_samples
 
+# Paths
 model_path = 'models/'
+data_path = 'data/mnist/images/'
+train_path = data_path + 'train'
+
+image_size = (28, 28)
+classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+batch_size = 1000
+epochs_count = 10
+
+train_data_generator = ImageDataGenerator(validation_split=0.1)
+
+train_directory_iterator = train_data_generator.flow_from_directory(
+    train_path,
+    target_size=image_size,
+    classes=classes,
+    batch_size=batch_size,
+    color_mode='grayscale',
+    subset='training'
+)
+validation_directory_iterator = train_data_generator.flow_from_directory(
+    train_path,
+    target_size=image_size,
+    classes=classes,
+    batch_size=batch_size,
+    color_mode='grayscale',
+    subset='validation'
+)
 
 model = Sequential([
-    Dense(units=16, input_shape=(1,), activation='relu'),
-    Dense(units=32, activation='relu'),
-    Dense(units=2, activation='softmax'),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    Flatten(),
+    Dense(10, activation='softmax'),
 ])
 
 model.summary()
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-train_samples, train_labels = get_train_samples()
-model.fit(x=train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
+model.fit(
+    train_directory_iterator,
+    validation_data=validation_directory_iterator,
+    steps_per_epoch=train_directory_iterator.samples / batch_size,
+    validation_steps=validation_directory_iterator.samples / batch_size,
+    epochs=epochs_count,
+    verbose=2
+)
 
 if not os.path.exists(model_path):
     os.makedirs(model_path)
